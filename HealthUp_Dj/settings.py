158e23a13e35a514899ee17c5d8a2cf92f725ba0
vedantsharma <vedantsharma441@gmail.com>
2019-04-14 11:59:05 +0530

    changes in settings.py

--- HealthUp_Dj/settings.py
+++ HealthUp_Dj/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_sloccount'
+    #'django_jenkins.tasks.run_jslint',
+    #'django_jenkins.tasks.run_csslint',
+    #'django_jenkins.tasks.run_sloccount'
 )
